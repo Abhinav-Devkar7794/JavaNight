0f8090b6b1ffc344c8e77fa04accb05f4a150843
Abhinav-Devkar7794 <devkarabhinav@gmail.com>
2019-01-29 22:27:27 +0530

    Add and Commit

---
+++ JavaTestPro/src/com/global/test/Student.java
@@ -0,0 +1,13 @@
+package com.global.test;
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Add and Commit With Push");
+		
+	}
+
+}
